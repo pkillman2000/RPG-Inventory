75b0dcdfc325dddef4b158a6d20fba40aa527fcf
Paul Killman <39015713+pkillman2000@users.noreply.github.com>
2026-07-31 13:13:26 -0500

    2 - Adding UI
    Basic Inventory UI
    Drag and Drop within Inventory UI

---
+++ Assets/Scripts/Utils/UI/Inventories/InventoryItemIcon.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameDevTV.UI.Inventories
+{
+    /// <summary>
+    /// To be put on the icon representing an inventory item. Allows the slot to
+    /// update the icon and number.
+    /// </summary>
+
+    /*
+     * This class simply changes the icon displayed for the item.
+    */
+    [RequireComponent(typeof(Image))]
+    public class InventoryItemIcon : MonoBehaviour
+    {
+        // PUBLIC
+
+        public void SetItem(Sprite item)
+        {
+            var iconImage = GetComponent<Image>();
+            if (item == null)
+            {
+                iconImage.enabled = false;
+            }
+            else
+            {
+                iconImage.enabled = true;
+                iconImage.sprite = item;
+            }
+        }
+
+        public Sprite GetItem()
+        {
+            var iconImage = GetComponent<Image>();
+            if (!iconImage.enabled)
+            {
+                return null;
+            }
+            return iconImage.sprite;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/UI/Inventories/InventorySlotUI.cs
@@ -0,0 +1,61 @@
+﻿using GameDevTV.Core.UI.Dragging;
+using UnityEngine;
+
+namespace GameDevTV.UI.Inventories
+{
+    public class InventorySlotUI : MonoBehaviour, IDragContainer<Sprite>
+    {
+        // CONFIG DATA
+        [SerializeField] InventoryItemIcon icon = null;
+
+        // PUBLIC
+
+        /*
+         * How many sprite items of this type can you accept?
+         * This is for stackable items like coins
+        */
+        public int MaxAcceptable(Sprite item)
+        {
+            if (GetItem() == null)
+            {
+                return int.MaxValue;
+            }
+            return 0;
+        }
+
+        /*
+         * Takes sprite and number of items.
+         * Designed for stackable items such as coins.
+        */
+        public void AddItems(Sprite item, int number)
+        {
+            icon.SetItem(item);
+        }
+
+        /*
+         * Returns icon of current item.
+        */
+        public Sprite GetItem()
+        {
+            return icon.GetItem();
+        }
+
+        /*
+         * Returns number of items in slot.
+         * Designed for stackable items.
+        */
+        public int GetNumber()
+        {
+            return 1;
+        }
+
+        /*
+         * Allows you to only move a certain
+         * number of items from slot.
+        */
+        public void RemoveItems(int number)
+        {
+            icon.SetItem(null);
+        }
+    }
+}
